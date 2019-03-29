4a289e94f6fa742bfe2e79ddcc9429ddccbcfcab
parvindavlatzoda <pdavlatzoda@bk.ru>
2019-03-29 14:56:01 +0500

    разметка для страницы управления

--- ClientApp/src/containers/Profile/ProfileInfoBar.js
+++ ClientApp/src/containers/Profile/ProfileInfoBar.js
@@ -7,20 +7,20 @@ class ProfileInfoBar extends Component {
     return (
       <Fragment>
 
-     <Row>
-       <Col span={9}>
-          <h1>your Info</h1>
-       </Col>
-       <Col span={5}>
-         Go passwordless  
-       </Col>
-       <Col span={5}>
-       Change your password
-       </Col>
-       <Col span={5}>
-       Manage your adress
-       </Col>
-     </Row>
+        <Row>
+          <Col span={9}>
+              <h1>your Info</h1>
+          </Col>
+          <Col span={5}>
+            Go passwordless  
+          </Col>
+          <Col span={5}>
+          Change your password
+          </Col>
+          <Col span={5}>
+          Manage your adress
+          </Col>
+        </Row>
     
      </Fragment> 
      
