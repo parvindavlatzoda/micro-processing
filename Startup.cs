e8bf642f8056a58f1e8ab5bfc7ed702c8063f349
Tim Nazar <timnazar@live.com>
2019-03-31 17:54:22 +0500

    mmm

--- Startup.cs
+++ Startup.cs
@@ -149,9 +149,11 @@ namespace MP {
                 app.UseDeveloperExceptionPage();
             }
             else {
-                app.UseExceptionHandler("/Error");
+                //app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
+                app.UseDeveloperExceptionPage();
+
             }
             
             AutoMapper.Mapper.Initialize(config => {
