939d4e73d2393e9327e471b23cc2e6a981490608
Tim Nazar <timnazar@live.com>
2019-03-31 19:46:46 +0500

    Added migration

---
+++ Data/Migrations/20190331144404_AddedStatusToRubReports.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MP.Data.Migrations
+{
+    public partial class AddedStatusToRubReports : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "RubReports",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "RubReports");
+        }
+    }
+}
