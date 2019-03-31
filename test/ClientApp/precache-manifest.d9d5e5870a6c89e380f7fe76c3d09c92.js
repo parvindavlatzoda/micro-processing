7ad29fee0532501a183a3109a2aae04f8068bdb1
Tim Nazar <timnazar@live.com>
2019-03-31 18:11:37 +0500

    correct publishing

---
+++ test/ClientApp/precache-manifest.d9d5e5870a6c89e380f7fe76c3d09c92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "941fb7c0ca18d7382bf5",
+    "url": "/static/css/main.dcec4032.chunk.css"
+  },
+  {
+    "revision": "941fb7c0ca18d7382bf5",
+    "url": "/static/js/main.06e34ffa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6e60a72429bc971149a",
+    "url": "/static/css/2.a46a3197.chunk.css"
+  },
+  {
+    "revision": "f6e60a72429bc971149a",
+    "url": "/static/js/2.2537f0ad.chunk.js"
+  },
+  {
+    "revision": "fc1d835f0ddf384b3d59144c08bf0d7e",
+    "url": "/static/media/user.fc1d835f.jpeg"
+  },
+  {
+    "revision": "f43627f821d0b5f5c400a139a689faa1",
+    "url": "/index.html"
+  }
+];
