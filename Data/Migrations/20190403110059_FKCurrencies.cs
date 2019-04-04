30cc01dbb67466943decba09a22965668609f4b2
Tim Nazar <timnazar@live.com>
2019-04-04 09:32:41 +0500

    Миграции для БД
    Создание таблиц Курсы и Валюты

---
+++ Data/Migrations/20190403110059_FKCurrencies.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MP.Data.Migrations
+{
+    public partial class FKCurrencies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CurrencyRates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Rate = table.Column<decimal>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CurrencyId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyRates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CurrencyRates_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyRates_CurrencyId",
+                table: "CurrencyRates",
+                column: "CurrencyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrencyRates");
+        }
+    }
+}
---
+++ Data/Migrations/20190403125519_UpdatedRates.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MP.Data.Migrations
+{
+    public partial class UpdatedRates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
