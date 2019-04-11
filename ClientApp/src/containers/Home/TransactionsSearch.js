d9d52c6be6ba188a2a07869b2ce24fc69f7f5217
parvindavlatzoda <pdavlatzoda@bk.ru>
2019-04-11 14:51:57 +0500

    button download

--- ClientApp/src/containers/Home/TransactionsSearch.js
+++ ClientApp/src/containers/Home/TransactionsSearch.js
@@ -9,10 +9,21 @@ const { MonthPicker, RangePicker } = DatePicker;
 const Option = Select.Option;
 
 
-const success = () => {
-  message.loading('Генерация отчета', 2)
+class TimeRelatedForm extends React.Component {
 
-  fetch('/api/1.0/keeper/reports/csv?serviceId=&from=&to', {
+state = {
+  services: [],
+  serviceId: null,
+  from: new Date(),
+  to: new Date(),
+}
+
+ success = () => {
+  message.loading('Генерация отчета', 2)
+  
+  const  { serviceId, from, to, selected } = this.state
+
+  fetch(`/api/1.0/keeper/reports/csv?serviceId=${serviceId}&from=${from}&to=${to}`, {
     })
       .then(res => res.blob()).then(blob => download(blob, 'report', 'text/csv'))
       .then(message.success('Отчет сгенерирован', 2))
@@ -23,32 +34,14 @@ const success = () => {
   
 };
 
-class TimeRelatedForm extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      services: [],
-    }
-
-    // fetch('/api/1.0/keeper/reports/csv', {
-    //   headers: {
-    //     'Authorization': `bearer ${Auth.getToken()}`,
-    //     'Content-type': 'application/json'
-    //   }
-    // })
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     this.setState({
-    //       transactions: data,
-    //       isLoading: false
-    //     });
-    //     console.log(data)
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
+  handleChange = selected => {
+    this.setState({ selected })
   }
+
+  handleChangeInput = event => {
+    const rate = event.target.value
+    this.setState({ rate })
+  }   
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -78,6 +71,7 @@ class TimeRelatedForm extends React.Component {
   }
 
   render() {
+    const { selected } = this.state
     const { getFieldDecorator } = this.props.form;
     const formItemLayout = {
       labelCol: {
@@ -89,6 +83,7 @@ class TimeRelatedForm extends React.Component {
         sm: { span: 8 },
       },
     };
+    
     const config = {
       rules: [{ type: 'object', required: false, message: 'Please select time!' }],
     };
@@ -97,68 +92,16 @@ class TimeRelatedForm extends React.Component {
     };
     return (
       <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-        {/* <Form.Item
-          label="Интервал"
-          style={{ margin: '0' }}
-        >
-          <Select
-            showSearch
-            style={{ width: 200 }}
-            placeholder="Выберите интервал"
-            defaultValue="jack"
-            optionFilterProp="children"
-            // onChange={handleChange}
-            // onFocus={handleFocus}
-            // onBlur={handleBlur}
-            filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-            disabled
-          >
-            <Option value="jack">За последний час</Option>
-            <Option value="lucy">Lucy</Option>
-            <Option value="tom">Tom</Option>
-          </Select>
-        </Form.Item> */}
+      
         <Form.Item
           label="Период"
           style={{ margin: '0' }}
         >
           {getFieldDecorator('range-time-picker', rangeConfig)(
-            <RangePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+            <RangePicker onChange={this.handleChangeInput} showTime format="YYYY-MM-DD HH:mm:ss" />
           )}
         </Form.Item>
-        {/* <Form.Item
-          label="Аккаунт"
-          style={{ margin: '0' }}
-        >
-            <Input placeholder="93 588 11 01" disabled />
-        </Form.Item>
-        <Form.Item
-          label="ID транзакции"
-          style={{ margin: '0' }}
-        >
-          <Input placeholder="9d22e8c4-56a9-409d-a9bf-82ca666f7761" disabled />
-        </Form.Item>
-        <Form.Item
-          label="Шлюз"
-          style={{ margin: '0' }}
-        >
-          <Select
-            showSearch
-            style={{ width: 200 }}
-            placeholder="Выберите статус"
-            defaultValue="all"
-            optionFilterProp="children"
-            // onChange={handleChange}
-            // onFocus={handleFocus}
-            // onBlur={handleBlur}
-            filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-            disabled
-          >
-            <Option value="all">Все</Option>
-            <Option value="lucy">Lucy</Option>
-            <Option value="tom">Tom</Option>
-          </Select> 
-        </Form.Item> */}
+      
         <Form.Item
           label="Service"
           style={{ margin: '0' }}
@@ -169,10 +112,9 @@ class TimeRelatedForm extends React.Component {
             placeholder="Выберите услугу"
             defaultValue="all"
             optionFilterProp="children"
-            // onChange={handleChange}
-            // onFocus={handleFocus}
-            // onBlur={handleBlur}
+
             filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            onChange={this.handleChange}
           >
             <Option value="all">Все</Option>
             <Option value="324">Билайн РТ (МБТ, рубль)</Option>
@@ -181,27 +123,7 @@ class TimeRelatedForm extends React.Component {
             <Option value="325">Тселл (МБТ, рубль)</Option>
           </Select>
         </Form.Item>
-        {/* <Form.Item
-          label="Status"
-          style={{ margin: '0' }}
-        >
-          <Select
-            showSearch
-            style={{ width: 200 }}
-            placeholder="Выберите статус"
-            defaultValue="all"
-            optionFilterProp="children"
-            // onChange={handleChange}
-            // onFocus={handleFocus}
-            // onBlur={handleBlur}
-            filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-            disabled
-          >
-            <Option value="all">Все</Option>
-            <Option value="lucy">Lucy</Option>
-            <Option value="tom">Tom</Option>
-          </Select>
-        </Form.Item> */}
+
         <Form.Item
           style={{ margin: '0' }}
           wrapperCol={{
@@ -209,8 +131,7 @@ class TimeRelatedForm extends React.Component {
             sm: { span: 16, offset: 3 },
           }}
         >
-          {/* <Button type="primary" htmlType="submit" style={{ marginRight: '1em' }} disabled>Search</Button> */}
-          <Button icon="download" onClick={success} htmlType="button">Download .csv</Button>
+          <Button icon="download" onClick={this.success} htmlType="button">Download .csv</Button>
         </Form.Item>
       </Form>
     );
