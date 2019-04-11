a77b262ed0e5bb7d29997cee6d245c17e4edc30b
parvindavlatzoda <pdavlatzoda@bk.ru>
2019-04-11 17:36:07 +0500

    nothing

--- ClientApp/src/containers/Home/TransactionsSearch.js
+++ ClientApp/src/containers/Home/TransactionsSearch.js
@@ -11,63 +11,49 @@ const Option = Select.Option;
 
 class TimeRelatedForm extends React.Component {
 
-state = {
-  services: [],
-  serviceId: null,
-  from: new Date(),
-  to: new Date(),
-}
+  state = {
+    services: [],
+    serviceId: null,
+    from: new Date(), 
+    to: new Date(),
+  }
 
- success = () => {
-  message.loading('Генерация отчета', 2)
-  
-  const  { serviceId, from, to, selected } = this.state
+  success = () => {
+    message.loading('Генерация отчета', 2)
+    
+    const  { serviceId, from, to, selected } = this.state
 
-  fetch(`/api/1.0/keeper/reports/csv?serviceId=${serviceId}&from=${from}&to=${to}`, {
-    })
-      .then(res => res.blob()).then(blob => download(blob, 'report', 'text/csv'))
-      .then(message.success('Отчет сгенерирован', 2))
-      .then(message.info('Дождитесь окончания загрузки файла', 2.5))
-      .catch((err) => {
-        console.log(err);
-      });
-  
-};
+    fetch(`/api/1.0/keeper/reports/csv?serviceId=${serviceId}&from=${from}&to=${to}`, {
+      })
+        .then(res => res.blob()).then(blob => download(blob, 'report', 'text/csv'))
+        .then(message.success('Отчет сгенерирован', 2))
+        .then(message.info('Дождитесь окончания загрузки файла', 2.5))
+        .catch((err) => {
+          console.log(err);
+        });
+    
+  };
 
   handleChange = selected => {
     this.setState({ selected })
   }
 
-  handleChangeInput = event => {
-    const rate = event.target.value
-    this.setState({ rate })
-  }   
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
+  handleSubmit = e => {
+//    const from = rangeTimeValue[0].format('DD.MM.YYYY')
+//    const to = rangeTimeValue[1].format('DD.MM.YYYY')
+//    this.setState({ from, to })
+  
+    e.preventDefault()
     this.props.form.validateFields((err, fieldsValue) => {
       if (err) {
-        return;
+        return
       }
-
-      // Should format date value before submit.
-      const rangeValue = fieldsValue['range-picker'];
-      const rangeTimeValue = fieldsValue['range-time-picker'];
-      const values = {
-        ...fieldsValue,
-        'date-picker': fieldsValue['date-picker'].format('YYYY-MM-DD'),
-        'date-time-picker': fieldsValue['date-time-picker'].format('YYYY-MM-DD HH:mm:ss'),
-        'month-picker': fieldsValue['month-picker'].format('YYYY-MM'),
-        'range-picker': [rangeValue[0].format('YYYY-MM-DD'), rangeValue[1].format('YYYY-MM-DD')],
-        'range-time-picker': [
-          rangeTimeValue[0].format('YYYY-MM-DD HH:mm:ss'),
-          rangeTimeValue[1].format('YYYY-MM-DD HH:mm:ss'),
-        ],
-        'time-picker': fieldsValue['time-picker'].format('HH:mm:ss'),
-      };
-      console.log('Received values of form: ', values);
-    });
+      const rangeTimeValue = fieldsValue['range-time-picker']
+      console.log(
+        rangeTimeValue[0].format('DD.MM.YYYY'),
+        rangeTimeValue[1].format('DD.MM.YYYY'),
+      )
+    })
   }
 
   render() {
@@ -83,7 +69,7 @@ state = {
         sm: { span: 8 },
       },
     };
-    
+
     const config = {
       rules: [{ type: 'object', required: false, message: 'Please select time!' }],
     };
@@ -98,7 +84,7 @@ state = {
           style={{ margin: '0' }}
         >
           {getFieldDecorator('range-time-picker', rangeConfig)(
-            <RangePicker onChange={this.handleChangeInput} showTime format="YYYY-MM-DD HH:mm:ss" />
+            <RangePicker onChange={this.handleChangeRange} showTime format="YYYY-MM-DD HH:mm:ss" />
           )}
         </Form.Item>
       
@@ -131,7 +117,7 @@ state = {
             sm: { span: 16, offset: 3 },
           }}
         >
-          <Button icon="download" onClick={this.success} htmlType="button">Download .csv</Button>
+          <Button icon="download"   htmlType="submit">Download .csv</Button>
         </Form.Item>
       </Form>
     );
